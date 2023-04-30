7aaaa66c9116e1f692e2043003b06df75453e274
uberphoeus <uberphoeus@gmail.com>
2023-04-30 09:31:17 +0900

    add 2-02 model

--- pybo/models.py
+++ pybo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    # 기존 모델을 속성으로 연결 / 연결된 Question이 삭제되면 답변도 삭제
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
