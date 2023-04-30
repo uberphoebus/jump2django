e8c4cd486ca27503310eb64d5d9e055d768106fd
uberphoeus <uberphoeus@gmail.com>
2023-05-01 00:23:12 +0900

    add 3-11 recommend

--- pybo/models.py
+++ pybo/models.py
@@ -4,17 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_question')
+    
+    def __str__(self):
+        return self.subject
 
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     # 기존 모델을 속성으로 연결 / 연결된 Question이 삭제되면 답변도 삭제
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+    voter = models.ManyToManyField(User, related_name='voter_answer')
--- pybo/views/answer_views.py
+++ pybo/views/answer_views.py
@@ -53,3 +53,13 @@ def answer_delete(request, answer_id):
     else:
         answer.delete()
     return redirect('pybo:detail', question_id=answer.question.id) # type: ignore
+
+
+@login_required(login_url='common:login')
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request=request, message='본인이 작성한 글은 추천할 수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+    return redirect('pybo:detail', question_id=answer.question.id) # type: ignore
