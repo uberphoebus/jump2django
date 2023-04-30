7229be0c128ef8eea330af8e534e71e9b7802f02
uberphoeus <uberphoeus@gmail.com>
2023-04-30 09:44:48 +0900

    add 2-04 admin

--- pybo/admin.py
+++ pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from pybo.models import Question
+
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    # 검색필드 subject 추가
+    search_fields = ['subject']
+
+# admin 사이트에 Question 모델을 등록 / 제목으로 검색할 수 있도록 설정
+admin.site.register(Question, QuestionAdmin)
