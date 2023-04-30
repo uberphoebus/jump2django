72986352127b311cc19a3db13e755e007ec6b460
uberphoeus <uberphoeus@gmail.com>
2023-04-30 10:32:51 +0900

    add 2-04 templates

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,4 +3,5 @@ from pybo import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:question_id>/', views.detail),
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from pybo.models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    # 순방향 create_date / 역방향 -create_date
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request=request, template_name='pybo/question_list.html', context=context)
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    # get_object_or_404는 객체가 없는 경우 404 페이지를 리턴
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request=request, template_name='pybo/question_detail.html', context=context)
